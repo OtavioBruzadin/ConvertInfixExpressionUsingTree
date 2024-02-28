dd3fbbd93833d89a9428d9d33e10b3ff4f0cb551
OtavioBruzadin <otavio.bruzadin2004@gmail.com>
2024-02-27 22:55:06 -0300

    Refactor:Made necessary changes due to changes on the Node structure

--- src/main/java/org/example/Tree.java
+++ src/main/java/org/example/Tree.java
@@ -3,100 +3,97 @@ package org.example;
 import java.util.Stack;
 
 public class Tree {
-    static Node newNode(char c)
-    {
-        Node n = new Node();
-        n.data = c;
-        n.left = n.right = null;
-        return n;
+    static Node newNode(char c){
+        Node node = new Node(c);
+        return node;
     }
 
     // Function to build Expression Tree
     static Node buildExpressionTree(String expression) {
 
         // Stack to hold nodes
-        Stack<Node> stN = new Stack<>();
+        Stack<Node> nodeStack = new Stack<>();
 
         // Stack to hold chars
-        Stack<Character> stC = new Stack<>();
+        Stack<Character> characterStack = new Stack<>();
         Node t, t1, t2;
 
         // Prioritising the operators
-        int []p = new int[123];
-        p['+'] = p['-'] = 1;
-        p['/'] = p['*'] = 2;
-        p['^'] = 3;
-        p[')'] = 0;
+        int []priorityRank = new int[123];
+        priorityRank['+'] = priorityRank['-'] = 1;
+        priorityRank['/'] = priorityRank['*'] = 2;
+        priorityRank['^'] = 3;
+        priorityRank[')'] = 0;
 
         for (int i = 0; i < expression.length(); i++)
         {
             if (expression.charAt(i) == '(') {
 
                 // Push '(' in char stack
-                stC.add(expression.charAt(i));
+                characterStack.add(expression.charAt(i));
             }
 
             // Push the operands in node stack
             else if (Character.isAlphabetic(expression.charAt(i)))
             {
                 t = newNode(expression.charAt(i));
-                stN.add(t);
+                nodeStack.add(t);
             }
-            else if (p[expression.charAt(i)] > 0)
+            else if (priorityRank[expression.charAt(i)] > 0)
             {
 
                 // If an operator with lower or
                 // same associativity appears
                 while (
-                        !stC.isEmpty() && stC.peek() != '('
-                                && ((expression.charAt(i) != '^' && p[stC.peek()] >= p[expression.charAt(i)])
+                        !characterStack.isEmpty() && characterStack.peek() != '('
+                                && ((expression.charAt(i) != '^' && priorityRank[characterStack.peek()] >= priorityRank[expression.charAt(i)])
                                 || (expression.charAt(i) == '^'
-                                && p[stC.peek()] > p[expression.charAt(i)])))
+                                && priorityRank[characterStack.peek()] > priorityRank[expression.charAt(i)])))
                 {
 
                     // Get and remove the top element
                     // from the character stack
-                    t = newNode(stC.peek());
-                    stC.pop();
+                    t = newNode(characterStack.peek());
+                    characterStack.pop();
 
                     // Get and remove the top element
                     // from the node stack
-                    t1 = stN.peek();
-                    stN.pop();
+                    t1 = nodeStack.peek();
+                    nodeStack.pop();
 
                     // Get and remove the currently top
                     // element from the node stack
-                    t2 = stN.peek();
-                    stN.pop();
+                    t2 = nodeStack.peek();
+                    nodeStack.pop();
 
                     // Update the tree
-                    t.left = t2;
-                    t.right = t1;
+                    t.setLeftSon(t2);
+                    t.setRightSon(t1);
 
                     // Push the node to the node stack
-                    stN.add(t);
+                    nodeStack.add(t);
                 }
 
                 // Push s[i] to char stack
-                stC.push(expression.charAt(i));
+                characterStack.push(expression.charAt(i));
             }
             else if (expression.charAt(i) == ')') {
-                while (!stC.isEmpty() && stC.peek() != '(')
+                while (!characterStack.isEmpty() && characterStack.peek() != '(')
                 {
-                    t = newNode(stC.peek());
-                    stC.pop();
-                    t1 = stN.peek();
-                    stN.pop();
-                    t2 = stN.peek();
-                    stN.pop();
-                    t.left = t2;
-                    t.right = t1;
-                    stN.add(t);
+                    t = newNode(characterStack.peek());
+                    characterStack.pop();
+                    t1 = nodeStack.peek();
+                    nodeStack.pop();
+                    t2 = nodeStack.peek();
+                    nodeStack.pop();
+                    t.setLeftSon(t2);
+                    t.setRightSon(t1);
+                    nodeStack.add(t);
                 }
-                stC.pop();
+                characterStack.pop();
             }
         }
-        t = stN.peek();
+        t = nodeStack.peek();
         return t;
     }
 
@@ -105,9 +102,9 @@ public class Tree {
             return resultString;
         }
 
-        resultString = inOrder(currentNode.left, resultString);
-        resultString = resultString + currentNode.data + " ";
-        resultString = inOrder(currentNode.right, resultString);
+        resultString = inOrder(currentNode.getLeftSon(), resultString);
+        resultString = resultString + currentNode.getValue()+ " ";
+        resultString = inOrder(currentNode.getRightSon(), resultString);
         return resultString;
     }
 
@@ -117,10 +114,10 @@ public static  String postOrder(Node currentNode, String resultString) {
         return resultString; // preserve previously calculated value
     }
     //Go through left
-    resultString = postOrder(currentNode.left, resultString);
+    resultString = postOrder(currentNode.getLeftSon(), resultString);
     //Go through right
-    resultString = postOrder(currentNode.right, resultString);
-    resultString = resultString + currentNode.data + " ";
+    resultString = postOrder(currentNode.getRightSon(), resultString);
+    resultString = resultString + currentNode.getValue() + " ";
     return resultString;
 }
 
@@ -129,11 +126,11 @@ public static  String postOrder(Node currentNode, String resultString) {
         {
             return resultString; // preserve previously calculated value
         }
-        resultString = resultString + currentNode.data + " ";
+        resultString = resultString + currentNode.getValue() + " ";
         //Go through left
-        resultString = preOrder(currentNode.left, resultString);
+        resultString = preOrder(currentNode.getLeftSon(), resultString);
         //Go through right
-        resultString = preOrder(currentNode.right, resultString);
+        resultString = preOrder(currentNode.getRightSon(), resultString);
 
         return resultString;
     }
