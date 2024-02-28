d501b37c88394ae46e315b6bca7e7dc542a1a53f
OtavioBruzadin <otavio.bruzadin2004@gmail.com>
2024-02-27 22:54:24 -0300

    Refactor:Changed the Node Structure to industry standards

--- src/main/java/org/example/Node.java
+++ src/main/java/org/example/Node.java
@@ -1,7 +1,48 @@
 package org.example;
 
 public class Node {
-    char data;
-    Node left,right;
 
+    private char value;
+//    private Node parentNode;
+
+    private Node leftSon;
+
+    private Node rightSon;
+
+    public Node (char data){
+        this.value = data;
+//      this.parentNode = parent;
+        this.leftSon =null;
+        this.rightSon = null;
+    }
+
+    public void setLeftSon(Node leftSon) {
+        this.leftSon = leftSon;
+    }
+
+    public void setRightSon(Node rightSon) {
+        this.rightSon = rightSon;
+    }
+
+//    public void setParentNode(Node parentNode) {
+//        this.parentNode = parentNode;
+//    }
+
+    public void setValue(char value) {
+        this.value = value;
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public Node getLeftSon() {
+        return leftSon;
+    }
+
+
+
+    public Node getRightSon() {
+        return rightSon;
+    }
 }
